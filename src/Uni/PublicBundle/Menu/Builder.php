4efb241bbd76f0e53307b9783a4beeba8da4fedb
Garri <gfiguero@gmail.com>
2017-08-24 23:57:31 +0000

    transformacion del sistema de rutas a una forma escalable

---
+++ src/Uni/PublicBundle/Menu/Builder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Uni\PublicBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Builder implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function topMenu(FactoryInterface $factory, array $options)
+    {
+
+        $page = $options['page'];
+
+        $topmenu = $factory->createItem('root');
+        $topmenu->setChildrenAttribute('class', 'nav navbar-nav navbar-nav-custom navbar-right');
+        $topmenu->setChildrenAttribute('id', 'topmenu');
+
+        $aboutLink = 'Nosotros';
+        if ($page->getAboutTitle()) $aboutLink = $page->getAboutTitle();
+        $featureLink = 'Características';
+        if ($page->getFeatureTitle()) $featureLink = $page->getFeatureTitle();
+        $contactLink = 'Contacto';
+        if ($page->getContactTitle()) $contactLink = $page->getContactTitle();
+
+        $topmenu->addChild($aboutLink, array('uri' => '#about'))->setLinkAttribute('class', 'page-scroll');
+        $topmenu->addChild($featureLink, array('uri' => '#feature'))->setLinkAttribute('class', 'page-scroll');
+        $topmenu->addChild($contactLink, array('uri' => '#contact'))->setLinkAttribute('class', 'page-scroll');
+
+        return $topmenu;
+    }
+
+}
