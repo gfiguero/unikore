aad460c5103573c0dcaed01d1f18f6d69f784086
Garri <gfiguero@gmail.com>
2017-10-29 18:51:31 +0000

    mejora de imagenes en catalogos públicos

--- src/Uni/PublicBundle/Controller/PageController.php
+++ src/Uni/PublicBundle/Controller/PageController.php
@@ -16,12 +16,35 @@ class PageController extends Controller
         $socialmedialist = $em->getRepository('UniAdminBundle:Socialmedia')->findByPage($page);
         $catalogs = $em->getRepository('UniAdminBundle:Catalog')->findByPage($page);
         shuffle($photographies);
+        $backgrounds = array();
+
+        if(!empty($photographies)) {
+            // conteo se secciones
+            $sections = 0;
+            if($page) $sections += 2;
+            if(!empty($features)) $sections += 1;
+            if(!empty($socialmedialist)) $sections += 1;
+            if(!empty($catalogs)) $sections += count($catalogs);
+
+            $currentPhotography = 0;
+            for ($section=0; $section < $sections; $section++) {
+                if (array_key_exists($currentPhotography, $photographies)) {
+                    array_push($backgrounds, $photographies[$currentPhotography]);
+                    $currentPhotography++;
+                } else {
+                    $currentPhotography = 0;
+                    array_push($backgrounds, $photographies[$currentPhotography]);
+                }
+            }
+        }
+
         return $this->render('UniPublicBundle:Page:index.html.twig', array(
             'features' => $features,
             'photographies' => $photographies,
             'socialmedialist' => $socialmedialist,
             'page' => $page,
             'catalogs' => $catalogs,
+            'backgrounds' => $backgrounds,
         ));
     }
 }
