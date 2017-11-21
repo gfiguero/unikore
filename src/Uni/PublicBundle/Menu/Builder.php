2b6166ec016e3b6a9e6fb9707f251ac431f3e629
Garri <gfiguero@gmail.com>
2017-11-21 22:01:24 +0000

    agregados enlaces externos

--- src/Uni/PublicBundle/Menu/Builder.php
+++ src/Uni/PublicBundle/Menu/Builder.php
@@ -20,22 +20,26 @@ class Builder implements ContainerAwareInterface
         $topmenu->setChildrenAttribute('class', 'nav navbar-nav navbar-nav-custom navbar-right');
         $topmenu->setChildrenAttribute('id', 'topmenu');
 
-        $aboutLink = 'Nosotros';
-        if ($page->getAboutTitle()) $aboutLink = $page->getAboutTitle();
-        $featureLink = 'Características';
-        if ($page->getFeatureTitle()) $featureLink = $page->getFeatureTitle();
-        $linkLink = 'Enlaces';
-        if ($page->getLinkTitle()) $linkLink = $page->getLinkTitle();
-        $socialLink = 'Social';
-        if ($page->getSocialmediaTitle()) $socialLink = $page->getSocialmediaTitle();
-        $contactLink = 'Contacto';
-        if ($page->getContactTitle()) $contactLink = $page->getContactTitle();
-
-        $topmenu->addChild($aboutLink, array('uri' => '#about'))->setLinkAttribute('class', 'page-scroll');
-        $topmenu->addChild($featureLink, array('uri' => '#feature'))->setLinkAttribute('class', 'page-scroll');
-        $topmenu->addChild($linkLink, array('uri' => '#link'))->setLinkAttribute('class', 'page-scroll');
-        $topmenu->addChild($socialLink, array('uri' => '#socialmedia'))->setLinkAttribute('class', 'page-scroll');
-        $topmenu->addChild($contactLink, array('uri' => '#contact'))->setLinkAttribute('class', 'page-scroll');
+        if ($page->getAboutTitle()) {
+            $aboutLink = $page->getAboutTitle();
+            $topmenu->addChild($aboutLink, array('uri' => '#about'))->setLinkAttribute('class', 'page-scroll');
+        }
+        if ($page->getFeatureTitle()) {
+            $featureLink = $page->getFeatureTitle();
+            $topmenu->addChild($featureLink, array('uri' => '#feature'))->setLinkAttribute('class', 'page-scroll');
+        }
+        if ($page->getLinkTitle()) {
+            $linkLink = $page->getLinkTitle();
+            $topmenu->addChild($linkLink, array('uri' => '#link'))->setLinkAttribute('class', 'page-scroll');
+        }
+        if ($page->getSocialmediaTitle()) {
+            $socialLink = $page->getSocialmediaTitle();
+            $topmenu->addChild($socialLink, array('uri' => '#socialmedia'))->setLinkAttribute('class', 'page-scroll');
+        }
+        if ($page->getContactTitle()) {
+            $contactLink = $page->getContactTitle();
+            $topmenu->addChild($contactLink, array('uri' => '#contact'))->setLinkAttribute('class', 'page-scroll');
+        }
 
         return $topmenu;
     }
