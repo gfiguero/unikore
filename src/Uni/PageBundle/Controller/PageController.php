67d8f7c61d45a8365fd1181499c9002457397085
Pablo <pablo.gaete08@gmail.com>
2018-02-15 23:04:39 -0300

    actualización de preview en controlador de Page

--- src/Uni/PageBundle/Controller/PageController.php
+++ src/Uni/PageBundle/Controller/PageController.php
@@ -194,22 +194,27 @@ class PageController extends Controller
     public function previewAction(Page $page)
     {
         $em = $this->getDoctrine()->getManager();
-        $page = $em->getRepository('UniAdminBundle:Page')->find($page);
+        $page = $em->getRepository('UniAdminBundle:Page')->findOneByDomain($this->getRequest()->getHost());
         $photographies = $em->getRepository('UniAdminBundle:Photography')->findByPage($page);
-        $features = $em->getRepository('UniAdminBundle:Feature')->findByPage($page);
         $teams = $em->getRepository('UniAdminBundle:Team')->findByPage($page);
-        $socialmedialist = $em->getRepository('UniAdminBundle:Socialmedia')->findByPage($page);
+        $features = $em->getRepository('UniAdminBundle:Feature')->findByPage($page);
+        $links = $em->getRepository('UniAdminBundle:Link')->findByPage($page);
         $catalogs = $em->getRepository('UniAdminBundle:Catalog')->findByPage($page);
+        $portfolios = $em->getRepository('UniAdminBundle:Portfolio')->findByPage($page);
+        $socialmedialist = $em->getRepository('UniAdminBundle:Socialmedia')->findByPage($page);
         shuffle($photographies);
         $backgrounds = array();
 
         if(!empty($photographies)) {
-            // conteo se secciones
+            // conteo de secciones
             $sections = 0;
-            if($page) $sections += 2;
+            if($page) $sections += 4; // (main, about, location, contact) = 4
+            if(!empty($teams)) $sections += 1;
             if(!empty($features)) $sections += 1;
-            if(!empty($socialmedialist)) $sections += 1;
+            if(!empty($links)) $sections += 1;
             if(!empty($catalogs)) $sections += count($catalogs);
+            if(!empty($portfolios)) $sections += count($portfolios);
+            if(!empty($socialmedialist)) $sections += 1;
 
             $currentPhotography = 0;
             for ($section=0; $section < $sections; $section++) {
@@ -222,14 +227,16 @@ class PageController extends Controller
                 }
             }
         }
-        
+
         return $this->render('UniPageBundle:Page:preview.html.twig', array(
-            'features' => $features,
-            'teams' => $teams,
-            'photographies' => $photographies,
-            'socialmedialist' => $socialmedialist,
             'page' => $page,
+            'photographies' => $photographies,
+            'teams' => $teams,
+            'features' => $features,
+            'links' => $links,
             'catalogs' => $catalogs,
+            'portfolios' => $portfolios,
+            'socialmedialist' => $socialmedialist,
             'backgrounds' => $backgrounds,
         ));
     }
